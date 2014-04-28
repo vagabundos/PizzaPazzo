b37be163829c4d3d76d88b9a7ac5fa009e0c2962
vagabundos <jonatasbazzoli@hotmail.com>
2014-04-28 19:09:19 -0300

    Criacao classe

---
+++ src/pizzapazzo/PizzaPazzo.java
@@ -0,0 +1,22 @@
+/*
+ * Falta criar os Dao para os objetos das classe que vão ser persistido no banco
+ * O banco de dados será o mysql-server pois é mais compativel com java
+ *  Vamos fazer as Interfaces usando java swing  
+ */
+
+package pizzapazzo;
+
+/**
+ *
+ * @author Gauss
+ */
+public class PizzaPazzo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
